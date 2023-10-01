3354933980ed6a7054ba4f1e2a6f6c944e3a4271
CherylZ4 <112735556+CherylZ4@users.noreply.github.com>
2023-10-01 16:21:42 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -5,11 +5,11 @@ public class Driver {
     public static void main(String [] args) throws IOException {
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double [] c1 = {6,5};
-        int [] e1 = {0,3};
+        double [] c1 = {4,5,6};
+        int [] e1 = {0,3,2};
         Polynomial p1 = new Polynomial(c1, e1);
-        double [] c2 = {-2,-9};
-        int [] e2 = { 1, 4};
+        double [] c2 = {-3,10};
+        int [] e2 = { 1, 2};
         Polynomial p2 = new Polynomial(c2, e2);
         Polynomial s = p2.add(p1);
         s.print();
@@ -18,11 +18,11 @@ public class Driver {
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
-        double[] c3 = {1,2,3};
-        int[] e3 = {1,3,4};
+        double[] c3 = {5,3,2};
+        int[] e3 = {5,1,0};
         Polynomial p3 = new Polynomial(c3, e3);
-        double[] c4 = {2,4};
-        int[] e4 = {0,1};
+        double[] c4 = {4,0};
+        int[] e4 = {2,4};
         Polynomial p4 = new Polynomial(c4, e4);
         Polynomial answer = p3.multiply(p4);
         for (int i = 0; i < answer.coefficients.length; i++){
